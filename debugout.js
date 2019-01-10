2705757096be47bac1529d19cef841097cefac74
Dave Anderson <nlclothing@gmail.com>
2019-01-10 12:01:12 -0500

    set debugout to class

--- debugout.js
+++ debugout.js
@@ -1,157 +1,189 @@
 /*
 
     debugout.js
-    by @inorganik
+	by @inorganik
+	revision by @abrenoch
     
 */
 
+const DEFAULT_FILENAME = 'debugout.txt';
+const MAX_LINES = 2500;
+const MAX_OBJECT_DEPTH = 25;
+
 // save all the console.logs
-function debugout() {
-	var self = this;
+class debugout {
 
-	// OPTIONS
-	self.realTimeLoggingOn = true; // log in real time (forwards to console.log)
-	self.useTimestamps = false; // insert a timestamp in front of each log
-	self.useLocalStorage = false; // store the output using window.localStorage() and continuously add to the same log each session
-	self.recordLogs = true; // set to false after you're done debugging to avoid the log eating up memory
-	self.autoTrim = true; // to avoid the log eating up potentially endless memory
-	self.maxLines = 2500; // if autoTrim is true, this many most recent lines are saved
-	self.tailNumLines = 100; // how many lines tail() will retrieve
-	self.logFilename = 'debugout.txt'; // filename of log downloaded with downloadLog()
-	self.maxDepth = 25; // max recursion depth for logged objects
+	constructor(maxLines = MAX_LINES, filename = DEFAULT_FILENAME, maxDepth = MAX_OBJECT_DEPTH) {
+		// OPTIONS
+		this.realTimeLoggingOn = true; // log in real time (forwards to console.log)
+		this.useTimestamps = false; // insert a timestamp in front of each log
+		this.useLocalStorage = false; // store the output using window.localStorage() and continuously add to the same log each session
+		this.recordLogs = true; // set to false after you're done debugging to avoid the log eating up memory
+		this.autoTrim = true; // to avoid the log eating up potentially endless memory
+		this.maxLines = maxLines; // if autoTrim is true, this many most recent lines are saved
+		this.tailNumLines = 100; // how many lines tail() will retrieve
+		this.logFilename = filename; // filename of log downloaded with downloadLog()
+		this.maxDepth = maxDepth; // max recursion depth for logged objects
 
-	// vars
-	self.depth = 0;
-	self.parentSizes = [0];
-	self.currentResult = '';
-	self.startTime = new Date();
-	self.output = '';
+		// vars
+		this.depth = 0;
+		this.parentSizes = [0];
+		this.currentResult = '';
+		this.startTime = new Date();
+		this.output = '';
 
-	this.version = function() { return '0.5.0' }
+		/*
+			START/RESUME LOG
+		*/
+		if (this.useLocalStorage) {
+			let saved = window.localStorage.getItem('debugout.js');
+			if (saved) {
+				saved = JSON.parse(saved);
+				this.output = saved.log;
+				let start = new Date(saved.startTime);
+				let end = new Date(saved.lastLog);
+				this.output += `\n---- Session end: ${saved.lastLog} ----\n`;
+				this.output += this.formatSessionDuration(start, end);
+				this.output += '\n\n';
+			}
+		}
+
+		this.output += `---- Session started: ${this.startTime} ----\n\n`;
+	}
+
+
+	version () { return '0.5.0' }
 
 	/*
 		USER METHODS
-	*/	
-	this.getLog = function() {
-		var retrievalTime = new Date();
+	*/
+	getLog () {
+		let retrievalTime = new Date();
 		// if recording is off, so dev knows why they don't have any logs
-		if (!self.recordLogs) {
-			self.log('[debugout.js] log recording is off.');
+		if (!this.recordLogs) {
+			this.log('[debugout.js] log recording is off.');
 		}
 		// if using local storage, get values
-		if (self.useLocalStorage) {
-			var saved = window.localStorage.getItem('debugout.js');
+		if (this.useLocalStorage) {
+			let saved = window.localStorage.getItem('debugout.js');
 			if (saved) {
 				saved = JSON.parse(saved);
-				self.startTime = new Date(saved.startTime);
-				self.output = saved.log;
+				this.startTime = new Date(saved.startTime);
+				this.output = saved.log;
 				retrievalTime = new Date(saved.lastLog);
 			}
 		}
-		return self.output
-			+ '\n---- Log retrieved: '+retrievalTime+' ----\n'
-			+ self.formatSessionDuration(self.startTime, retrievalTime);
+		return this.output
+			+ '\n---- Log retrieved: ' + retrievalTime + ' ----\n'
+			+ this.formatSessionDuration(this.startTime, retrievalTime);
 	}
+
 	// accepts optional number or uses the default for number of lines
-	this.tail = function(numLines) {
-		var numLines = numLines || self.tailLines;
-		return self.trimLog(self.getLog(), numLines);
+	tail (numLines) {
+		let numLines = numLines || this.tailLines;
+		return this.trimLog(this.getLog(), numLines);
 	}
+
 	// accepts a string to search for
-	this.search = function(string) {
-		var lines = self.output.split('\n');
-		var rgx = new RegExp(string);
-		var matched = [];
+	search (string) {
+		let lines = this.output.split('\n');
+		let rgx = new RegExp(string);
+		let matched = [];
 		// can't use a simple Array.prototype.filter() here
 		// because we need to add the line number
-		for (var i = 0; i < lines.length; i++) {
-			var addr = '['+i+'] ';
+		for (let i = 0; i < lines.length; i++) {
+			let addr = `[${i}] `;
 			if (lines[i].match(rgx)) {
 				matched.push(addr + lines[i]);
 			}
 		}
-		var result = matched.join('\n');
-		if (result.length == 0) result = 'Nothing found for "'+string+'".';
+		let result = matched.join('\n');
+		if (result.length == 0) result = `Nothing found for "${string}".`;
 		return result
 	}
+
 	// accepts the starting line and how many lines after the starting line you want
-	this.getSlice = function(lineNumber, numLines) {
-		var lines = self.output.split('\n');
-		var segment = lines.slice(lineNumber, lineNumber + numLines);
+	getSlice (lineNumber, numLines) {
+		let lines = this.output.split('\n');
+		let segment = lines.slice(lineNumber, lineNumber + numLines);
 		return segment.join('\n');
 	}
+
 	// immediately downloads the log - for desktop browser use
-	this.downloadLog = function() {
-	    var logFile = self.getLog();
-            var blob = new Blob([logFile], { type: 'data:text/plain;charset=utf-8' });
-            var a = document.createElement('a');
-            a.href = window.URL.createObjectURL(blob);
-            a.target = '_blank';
-            a.download = self.logFilename;
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(a.href);
+	downloadLog () {
+		let logFile = this.getLog();
+		let blob = new Blob([logFile], { type: 'data:text/plain;charset=utf-8' });
+		let a = document.createElement('a');
+		a.href = window.URL.createObjectURL(blob);
+		a.target = '_blank';
+		a.download = this.logFilename;
+		document.body.appendChild(a);
+		a.click();
+		document.body.removeChild(a);
+		window.URL.revokeObjectURL(a.href);
 	}
+
 	// clears the log
-	this.clear = function() {
-		var clearTime = new Date();
-		self.output = '---- Log cleared: '+clearTime+' ----\n';
-		if (self.useLocalStorage) {
+	clear () {
+		let clearTime = new Date();
+		this.output = `---- Log cleared: ${clearTime} ----\n`;
+		if (this.useLocalStorage) {
 			// local storage
-			var saveObject = {
-				startTime: self.startTime,
-				log: self.output,
+			let saveObject = {
+				startTime: this.startTime,
+				log: this.output,
 				lastLog: clearTime
 			}
 			saveObject = JSON.stringify(saveObject);
 			window.localStorage.setItem('debugout.js', saveObject);
 		}
-		if (self.realTimeLoggingOn) console.log('[debugout.js] clear()');
+		if (this.realTimeLoggingOn) console.log('[debugout.js] clear()');
 	}
+
 	// records a log
-	this.log = function(obj) {
+	log (obj) {
 		// log in real time
-		if (self.realTimeLoggingOn) console.log(obj);
+		if (this.realTimeLoggingOn) console.log(obj);
 		// record log
-		var type = self.determineType(obj);
-		if (type != null && self.recordLogs) {
-			var addition = self.formatType(type, obj);
+		let type = this.determineType(obj);
+		if (type != null && this.recordLogs) {
+			let addition = this.formatType(type, obj);
 			// timestamp, formatted for brevity
-			if (self.useTimestamps) {
-				var logTime = new Date();
-				self.output += self.formatTimestamp(logTime);
+			if (this.useTimestamps) {
+				let logTime = new Date();
+				this.output += this.formatTimestamp(logTime);
 			}
-			self.output += addition+'\n';
-			if (self.autoTrim) self.output = self.trimLog(self.output, self.maxLines);
+			this.output += addition + '\n';
+			if (this.autoTrim) this.output = this.trimLog(this.output, this.maxLines);
 			// local storage
-			if (self.useLocalStorage) {
-				var last = new Date();
-				var saveObject = {
-					startTime: self.startTime,
-					log: self.output,
+			if (this.useLocalStorage) {
+				let last = new Date();
+				let saveObject = {
+					startTime: this.startTime,
+					log: this.output,
 					lastLog: last
 				}
 				saveObject = JSON.stringify(saveObject);
 				window.localStorage.setItem('debugout.js', saveObject);
 			}
 		}
-		self.depth = 0;
-		self.parentSizes = [0];
-		self.currentResult = '';
+		this.depth = 0;
+		this.parentSizes = [0];
+		this.currentResult = '';
 	}
+
 	/*
 		METHODS FOR CONSTRUCTING THE LOG
 	*/
 
 	// like typeof but classifies objects of type 'object'
 	// kept separate from formatType() so you can use at your convenience!
-	this.determineType = function(object) {
+	determineType (object) {
 		if (object != null) {
-			var typeResult;
-			var type = typeof object;
+			let typeResult;
+			let type = typeof object;
 			if (type == 'object') {
-				var len = object.length;
+				let len = object.length;
 				if (len == null) {
 					if (typeof object.getTime == 'function') {
 						typeResult = 'Date';
@@ -173,153 +205,143 @@ function debugout() {
 			return null;
 		}
 	}
+
 	// format type accordingly, recursively if necessary
-	this.formatType = function(type, obj) {
-		if (self.maxDepth && self.depth >= self.maxDepth) {
+	formatType (type, obj) {
+		if (this.maxDepth && this.depth >= this.maxDepth) {
 			return '... (max-depth reached)';
 		}
 
-		switch(type) {
-			case 'Object' :
-				self.currentResult += '{\n';
-				self.depth++;
-				self.parentSizes.push(self.objectSize(obj));
-				var i = 0;
-				for (var prop in obj) {
-					self.currentResult += self.indentsForDepth(self.depth);
-					self.currentResult += prop + ': ';
-					var subtype = self.determineType(obj[prop]);
-					var subresult = self.formatType(subtype, obj[prop]);
+		switch (type) {
+			case 'Object':
+				this.currentResult += '{\n';
+				this.depth++;
+				this.parentSizes.push(this.objectSize(obj));
+				let i = 0;
+				for (let prop in obj) {
+					this.currentResult += this.indentsForDepth(this.depth);
+					this.currentResult += prop + ': ';
+					let subtype = this.determineType(obj[prop]);
+					let subresult = this.formatType(subtype, obj[prop]);
 					if (subresult) {
-						self.currentResult += subresult;
-						if (i != self.parentSizes[self.depth]-1) self.currentResult += ',';
-						self.currentResult += '\n';
+						this.currentResult += subresult;
+						if (i != this.parentSizes[this.depth] - 1) this.currentResult += ',';
+						this.currentResult += '\n';
 					} else {
-						if (i != self.parentSizes[self.depth]-1) self.currentResult += ',';
-						self.currentResult += '\n';
+						if (i != this.parentSizes[this.depth] - 1) this.currentResult += ',';
+						this.currentResult += '\n';
 					}
 					i++;
 				}
-				self.depth--;
-				self.parentSizes.pop();
-				self.currentResult += self.indentsForDepth(self.depth);
-				self.currentResult += '}';
-				if (self.depth == 0) return self.currentResult;
+				this.depth--;
+				this.parentSizes.pop();
+				this.currentResult += this.indentsForDepth(this.depth);
+				this.currentResult += '}';
+				if (this.depth == 0) return this.currentResult;
 				break;
-			case 'Array' :
-				self.currentResult += '[';
-				self.depth++;
-				self.parentSizes.push(obj.length);
-				for (var i = 0; i < obj.length; i++) {
-					var subtype = self.determineType(obj[i]);
-					if (subtype == 'Object' || subtype == 'Array') self.currentResult += '\n' + self.indentsForDepth(self.depth);
-					var subresult = self.formatType(subtype, obj[i]);
+			case 'Array':
+				this.currentResult += '[';
+				this.depth++;
+				this.parentSizes.push(obj.length);
+				for (let i = 0; i < obj.length; i++) {
+					let subtype = this.determineType(obj[i]);
+					if (subtype == 'Object' || subtype == 'Array') this.currentResult += '\n' + this.indentsForDepth(this.depth);
+					let subresult = this.formatType(subtype, obj[i]);
 					if (subresult) {
-						self.currentResult += subresult;
-						if (i != self.parentSizes[self.depth]-1) self.currentResult += ', ';
-						if (subtype == 'Array') self.currentResult += '\n';
+						this.currentResult += subresult;
+						if (i != this.parentSizes[this.depth] - 1) this.currentResult += ', ';
+						if (subtype == 'Array') this.currentResult += '\n';
 					} else {
-						if (i != self.parentSizes[self.depth]-1) self.currentResult += ', ';
-						if (subtype != 'Object') self.currentResult += '\n';
-						else if (i == self.parentSizes[self.depth]-1) self.currentResult += '\n';
+						if (i != this.parentSizes[this.depth] - 1) this.currentResult += ', ';
+						if (subtype != 'Object') this.currentResult += '\n';
+						else if (i == this.parentSizes[this.depth] - 1) this.currentResult += '\n';
 					}
 				}
-				self.depth--;
-				self.parentSizes.pop();
-				self.currentResult += ']';
-				if (self.depth == 0) return self.currentResult;
+				this.depth--;
+				this.parentSizes.pop();
+				this.currentResult += ']';
+				if (this.depth == 0) return this.currentResult;
 				break;
-			case 'function' :
+			case 'function':
 				obj += '';
-				var lines = obj.split('\n');
-				for (var i = 0; i < lines.length; i++) {
-					if (lines[i].match(/\}/)) self.depth--;
-					self.currentResult += self.indentsForDepth(self.depth);
-					if (lines[i].match(/\{/)) self.depth++;
-					self.currentResult += lines[i] + '\n';
+				let lines = obj.split('\n');
+				for (let i = 0; i < lines.length; i++) {
+					if (lines[i].match(/\}/)) this.depth--;
+					this.currentResult += this.indentsForDepth(this.depth);
+					if (lines[i].match(/\{/)) this.depth++;
+					this.currentResult += lines[i] + '\n';
 				}
-				return self.currentResult;
-				break;
-			case 'RegExp' :
-				return '/'+obj.source+'/';
-				break;
-			case 'Date' :
-			case 'string' : 
-				if (self.depth > 0 || obj.length == 0) {
-					return '"'+obj+'"';
+				return this.currentResult;
+			case 'RegExp':
+				return '/' + obj.source + '/';
+			case 'Date':
+			case 'string':
+				if (this.depth > 0 || obj.length == 0) {
+					return '"' + obj + '"';
 				} else {
 					return obj;
 				}
-			case 'boolean' :
+			case 'boolean':
 				if (obj) return 'true';
 				else return 'false';
-			case 'number' :
-				return obj+'';
-				break;
+			case 'number':
+				return obj + '';
 		}
 	}
-	this.indentsForDepth = function(depth) {
-		var str = '';
-		for (var i = 0; i < depth; i++) {
+
+	indentsForDepth (depth) {
+		let str = '';
+		for (let i = 0; i < depth; i++) {
 			str += '\t';
 		}
 		return str;
 	}
-	this.trimLog = function(log, maxLines) {
-		var lines = log.split('\n');
+
+	trimLog (log, maxLines) {
+		let lines = log.split('\n');
 		if (lines.length > maxLines) {
 			lines = lines.slice(lines.length - maxLines);
 		}
 		return lines.join('\n');
 	}
-	this.lines = function() {
-		return self.output.split('\n').length;
-	}
-	// calculate testing time
-	this.formatSessionDuration = function(startTime, endTime) {
-		var msec = endTime - startTime;
-		var hh = Math.floor(msec / 1000 / 60 / 60);
-		var hrs = ('0' + hh).slice(-2);
-		msec -= hh * 1000 * 60 * 60;
-		var mm = Math.floor(msec / 1000 / 60);
-		var mins = ('0' + mm).slice(-2);
-		msec -= mm * 1000 * 60;
-		var ss = Math.floor(msec / 1000);
-		var secs = ('0' + ss).slice(-2);
-		msec -= ss * 1000;
-		return '---- Session duration: '+hrs+':'+mins+':'+secs+' ----'
-	}
-	this.formatTimestamp = function(timestamp) {
-		var year = timestamp.getFullYear();
-		var date = timestamp.getDate();
-		var month = ('0' + (timestamp.getMonth() +1)).slice(-2);
-		var hrs = Number(timestamp.getHours()); 
-		var mins = ('0' + timestamp.getMinutes()).slice(-2);
-		var secs = ('0' + timestamp.getSeconds()).slice(-2);
-		return '['+ year + '-' + month + '-' + date + ' ' + hrs + ':' + mins + ':'+secs + ']: ';
-	}
-	this.objectSize = function(obj) {
-	    var size = 0, key;
-	    for (key in obj) {
-	        if (obj.hasOwnProperty(key)) size++;
-	    }
-	    return size;
+
+	lines () {
+		return this.output.split('\n').length;
 	}
 
-	/*
-		START/RESUME LOG
-	*/
-	if (self.useLocalStorage) {
-		var saved = window.localStorage.getItem('debugout.js');
-		if (saved) {
-			saved = JSON.parse(saved);
-			self.output = saved.log;
-			var start = new Date(saved.startTime);
-			var end = new Date(saved.lastLog);
-			self.output += '\n---- Session end: '+saved.lastLog+' ----\n';
-			self.output += self.formatSessionDuration(start, end);
-			self.output += '\n\n';
+	// calculate testing time
+	formatSessionDuration (startTime, endTime) {
+		let msec = endTime - startTime;
+		let hh = Math.floor(msec / 1000 / 60 / 60);
+		let hrs = ('0' + hh).slice(-2);
+		msec -= hh * 1000 * 60 * 60;
+		let mm = Math.floor(msec / 1000 / 60);
+		let mins = ('0' + mm).slice(-2);
+		msec -= mm * 1000 * 60;
+		let ss = Math.floor(msec / 1000);
+		let secs = ('0' + ss).slice(-2);
+		msec -= ss * 1000;
+		return `---- Session duration: ${hrs}:${mins}:${secs} ----`;
+	}
+
+	formatTimestamp (timestamp) {
+		let year = timestamp.getFullYear();
+		let date = timestamp.getDate();
+		let month = ('0' + (timestamp.getMonth() + 1)).slice(-2);
+		let hrs = Number(timestamp.getHours());
+		let mins = ('0' + timestamp.getMinutes()).slice(-2);
+		let secs = ('0' + timestamp.getSeconds()).slice(-2);
+		return `[${year}-${month}-${date} ${hrs}:${mins}:${secs}]: `;
+	}
+
+	objectSize (obj) {
+		let size = 0, key;
+		for (key in obj) {
+			if (obj.hasOwnProperty(key)) size++;
 		}
-	} 
-	self.output += '---- Session started: '+self.startTime+' ----\n\n';
-}
+		return size;
+	}
+};
+
+export default debugout;
+export { debugout };
